1a05481e1887c79a8bf87b95016cda706ff0365f
Ostap Chichiginarov <ostap00034@gmail.com>
2024-12-14 03:15:27 +0900

    fix in password reset

--- src/components/screens/password-reset.tsx
+++ src/components/screens/password-reset.tsx
@@ -1,20 +1,25 @@
+import Loader from "../ui/loader"
+
 import { useState } from "react"
 import { Button } from "../ui/button"
 import { Input } from "../ui/input"
 import { useMutation } from "@tanstack/react-query"
 import { AuthService } from "@/services/auth/auth"
-import Loader from "../ui/loader"
 import { useNavigate } from "react-router-dom"
 
 const PasswordResetScreen = () => {
-  const [email, setEmail] = useState<string>('')
+  const [email, setEmail] = useState<string>("")
+  const [errors, setErrors] = useState<string[]>([])
 
   const navigate = useNavigate()
 
-  const {mutate, isPending} = useMutation({
-    mutationKey: ['reset password'],
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["reset password"],
     mutationFn: async () => await AuthService.resetPassword(email),
-    onSuccess: () => navigate('/password-reset-wait')
+    onSuccess: () => navigate("/password-reset-wait"),
+    onError: (e: any) => {
+      setErrors(e.response.data.details)
+    },
   })
 
   if (isPending) return <Loader />
@@ -25,14 +30,22 @@ const PasswordResetScreen = () => {
         Для восстановления вашего пароля введите адрес вашей электронной почты,
         которая зарегистрирована в системе.
       </div>
-      <div className="flex flex-row justify-start gap-6">
+      <div className="flex flex-col justify-start gap-6">
         <Input
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           type="email"
           className="w-[200px]"
           placeholder="Ваша электронная почта"
         />
+        <div className="flex flex-col gap-1">
+          {errors.length > 0
+            ? errors.map((error) => (
+                <div className="text-red-500 text-sm">{error}</div>
+              ))
+            : null}
+        </div>
         <Button onClick={() => mutate()}>Восстановить</Button>
       </div>
     </div>
